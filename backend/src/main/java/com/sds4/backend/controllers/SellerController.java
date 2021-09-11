53721ff4bc5b0879c313a6dd2444cfdddee140d6
srportto <ceeh1100@gmail.com>
2021-09-11 08:05:11 -0300

    implementacao da consulta paginada

--- backend/src/main/java/com/sds4/backend/controllers/SellerController.java
+++ backend/src/main/java/com/sds4/backend/controllers/SellerController.java
@@ -3,12 +3,12 @@ package com.sds4.backend.controllers;
 import com.sds4.backend.dtos.response.SellerDTO;
 import com.sds4.backend.services.SellerService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sellers")
@@ -17,16 +17,10 @@ public class SellerController {
 
     private final SellerService service;
 
-//    @GetMapping
-//    public ResponseEntity<Page<UsuarioDTO>> findAll(Pageable pageable){
-//        Page<UsuarioDTO> pageClient = service.findAllPaged(pageable);
-//        return ResponseEntity.ok().body(pageClient);
-//    }
-
     @GetMapping
-    public ResponseEntity<List<SellerDTO>> findAll(){
-        List<SellerDTO> sellerList = service.findAll();
-        return ResponseEntity.ok().body(sellerList);
+    public ResponseEntity<Page<SellerDTO>> findAll(Pageable pageable){
+        Page<SellerDTO> pageClient = service.findAllPaged(pageable);
+        return ResponseEntity.ok().body(pageClient);
     }
 
 }
--- backend/src/main/java/com/sds4/backend/services/SellerService.java
+++ backend/src/main/java/com/sds4/backend/services/SellerService.java
@@ -4,19 +4,18 @@ import com.sds4.backend.dtos.response.SellerDTO;
 import com.sds4.backend.models.entities.Seller;
 import com.sds4.backend.repositories.SellerRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class SellerService {
     private final SellerRepository sellerRepository;
 
-    public List<SellerDTO> findAll(){
-        List<Seller> sellersList = sellerRepository.findAll();
-        var result =  sellersList.stream().map(seller -> new SellerDTO(seller)).collect(Collectors.toList());
+    public Page<SellerDTO> findAllPaged(Pageable pageable){
+        Page<Seller> sellersList = sellerRepository.findAll(pageable);
+        var result =  sellersList.map(seller -> new SellerDTO(seller));
         return result;
     }
 }
