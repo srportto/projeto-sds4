71476d801320089488ca85d1aad4cbe2890cea03
srportto <ceeh1100@gmail.com>
2021-09-18 13:59:36 -0300

    edicao variavel - hack para execucao local e no netlify

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,7 @@
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+/* 
+  ?? -> Eh um operador de validacao de nulidade, caso a variavel REACT_APP_BACKEND_URL não exista no ambiente
+     passa-se o valor a direita de "??" , no caso: http://localhost:8080"
+
+     Esse hack permite roda a aplicação local e no netlify
+*/
